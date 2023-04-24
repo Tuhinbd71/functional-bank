cc7352bcd6c555d2efa95406b7a549af838c6a2a
Tuhinbd71 <m.tuhin02bd@gmail.com>
2023-04-24 17:11:18 +0600

    new commit

--- js/banking.js
+++ js/banking.js
@@ -1,3 +1,18 @@
+// user login Form section
+document.getElementById("submit-btn").addEventListener("click", function () {
+    // get user email
+    const inputField = document.getElementById("user-email");
+    userEmail = inputField.value;
+    // get user password
+    const userPassword = document.getElementById("user-password");
+    const userPasswordValue = userPassword.value;
+    if (userEmail == "m.tuhinbd@gmail.com" && userPasswordValue == "THN@00dotcom") {
+        window.location.href = "banking.html"
+    }
+    else {
+        console.log("Please! input valid value")
+    }
+})
 // Get update deposit & withdraw and balance using function.........
 function getInputValue(inputId) {
     const inputField = document.getElementById(inputId);
@@ -5,4 +20,12 @@ function getInputValue(inputId) {
     const inputFieldFloatValue = parseFloat(inputFieldValue);
     inputField.value = "";
     return inputFieldFloatValue;
+}
+// get update Deposit & withdraw field using function.......
+function depositWithdrawField(depositWithdrawAdd, newDepositValue) {
+    const depositField = document.getElementById(depositWithdrawAdd);
+    const previousDepositAmount = depositField.innerText;
+    const previousDepositAmountValue = parseFloat(previousDepositAmount);
+    const newDdepositTotal = previousDepositAmountValue + newDepositValue;
+    depositField.innerText = newDdepositTotal;
 }
